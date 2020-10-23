8412431e29f7d779b1498938d65d553444a1d5f1
Arijit20 <ARIJIT DEY@LAPTOP-F4J8SDAU>
2020-10-23 21:30:58 +0530

    [Arijit]Added : Method Added And Tested To Calculate Fare

--- src/test/java/com/cg/InvoiceServiceTest.java
+++ src/test/java/com/cg/InvoiceServiceTest.java
@@ -6,6 +6,22 @@ import com.cg.InvoiceGenerator;
 
 import static org.junit.Assert.*;
 
+import org.junit.Assert;
+import org.junit.Before;
+
 public class InvoiceServiceTest {
-    
+InvoiceGenerator invoiceGenerator;
+	
+	@Before
+	public void setUp() {
+		 invoiceGenerator = new InvoiceGenerator();
+	}
+	
+	@Test
+	public void givenDistanceTimeReturnFare() {
+		double distance = 2.0;
+		int time = 5;
+		double fare = invoiceGenerator.calculateFare(distance, time);
+		Assert.assertEquals(25, fare, 0.0);
+	}
 }
