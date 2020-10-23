f18f49f912de69b85a80e5d61abc67583b10b5b2
Arijit20 <ARIJIT DEY@LAPTOP-F4J8SDAU>
2020-10-23 21:33:00 +0530

    [Arijit]Modified : Fare Calculation Method Modified To Include Min Fare

--- src/test/java/com/cg/InvoiceServiceTest.java
+++ src/test/java/com/cg/InvoiceServiceTest.java
@@ -24,4 +24,12 @@ InvoiceGenerator invoiceGenerator;
 		double fare = invoiceGenerator.calculateFare(distance, time);
 		Assert.assertEquals(25, fare, 0.0);
 	}
+	
+	@Test
+	public void givenLessDistanceTimeReturnMinFare() {
+		double distance = 0.1;
+		int time = 1;
+		double fare = invoiceGenerator.calculateFare(distance, time);
+		Assert.assertEquals(5, fare, 0.0);
+	}
 }
