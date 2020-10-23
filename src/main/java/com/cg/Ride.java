6b3038c69711783b45ce8abd3d6a4934c84180ba
Arijit20 <ARIJIT DEY@LAPTOP-F4J8SDAU>
2020-10-23 21:39:42 +0530

    [Arijit]Added : Ride Class Added To Calculate Fare For Multiple Rides

---
+++ src/main/java/com/cg/Ride.java
@@ -0,0 +1,11 @@
+package com.cg;
+
+public class Ride {
+	public double distnce;
+	public int time;
+  
+	public Ride(double distance, int time) {
+		this.distnce = distance;
+		this.time = time;
+	}
+}
